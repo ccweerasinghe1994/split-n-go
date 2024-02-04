61be663879dd520ddae70284d93e4c812ef2f83d
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 09:54:37 +0530

    Refactor expense calculation logic and add emoji labels

--- src/components/FormSplitBill/FormSplittBill.tsx
+++ src/components/FormSplitBill/FormSplittBill.tsx
@@ -6,17 +6,22 @@ const FormSplitBill: FC<IFormSplitBillProps> = ({ friend }) => {
     const [totalBill, setTotalBill] = useState<number>(0);
     const [yourExpense, setYourExpense] = useState<number>(0);
     const [friendExpense, setFriendExpense] = useState<number>(0);
-
+    const [whoIsPaying, setWhoIsPaying] = useState<string>("you");
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
         const id = e.target.id;
 
-        if (id === "split") setYourExpense(Number(value));
-        else setTotalBill(Number(value));
+        if (id === "split") {
+            if (Number(value) > totalBill) {
+                return;
+            }
+            setYourExpense(Number(value));
+        } else setTotalBill(Number(value));
     }
 
     useEffect(() => {
-        setFriendExpense(totalBill - yourExpense);
+        const friendExpenses = totalBill - yourExpense;
+        setFriendExpense(friendExpenses);
     }, [totalBill, yourExpense]);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,23 +30,28 @@ const FormSplitBill: FC<IFormSplitBillProps> = ({ friend }) => {
         console.log("Form Submitted");
     }
 
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        setWhoIsPaying(value);
+    }
+
     return (
         <form onSubmit={handleSubmit} className="form-split-bill">
             <h2>Split the Bill with {friend?.name}</h2>
 
-            <label htmlFor="split">Bill Value</label>
+            <label htmlFor="split">💰 Bill Value</label>
             <input onChange={handleChange} value={totalBill} type="number" id="total" name="split" />
 
-            <label htmlFor="split">Your Expenses</label>
+            <label htmlFor="split">😊 Your Expenses</label>
             <input value={yourExpense} onChange={handleChange} type="number" id="split" name="split" />
 
-            <label htmlFor="split">{friend?.name}'s expense</label>
+            <label htmlFor="split">🙍‍♂️ {friend?.name}'s expense</label>
             <input value={friendExpense} type="number" id="paying" name="split" disabled />
 
-            <label htmlFor="">Who is paying the bill</label>
-            <select name="" id="">
+            <label htmlFor="">💵 Who is paying the bill</label>
+            <select value={whoIsPaying} onChange={handleSelect}>
                 <option value="you">You</option>
-                <option value="x">{friend?.name}</option>
+                <option value="friend">{friend?.name}</option>
             </select>
 
             <Button>Split The Bill</Button>
