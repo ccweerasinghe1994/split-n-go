8db42dcaf563c2a7a06c70318a8a1c26ffbb584e
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 11:13:19 +0530

    Add handleBillUpdate prop to IFormSplitBillProps

--- src/components/FormSplitBill/types.tsx
+++ src/components/FormSplitBill/types.tsx
@@ -2,4 +2,5 @@ import { IFriend } from "../FriendList/types";
 
 export interface IFormSplitBillProps {
     friend: IFriend | null;
+    handleBillUpdate: (friendId: number, yourExpense: number) => void;
 }
