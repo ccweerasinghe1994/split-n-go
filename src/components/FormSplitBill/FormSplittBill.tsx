15a4ee5a8239f275cba60de58530a9700c776205
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 07:52:14 +0530

    Add split bill functionality to FormSplitBill component

--- src/components/FormSplitBill/FormSplittBill.tsx
+++ src/components/FormSplitBill/FormSplittBill.tsx
@@ -1,21 +1,49 @@
+import { FC, useEffect, useState } from "react";
 import Button from "../Button/Button";
+import { IFormSplitBillProps } from "./types";
 
-const FormSplitBill = () => {
+const FormSplitBill: FC<IFormSplitBillProps> = ({ friend }) => {
+    const [totalBill, setTotalBill] = useState<number>(0);
+    const [yourExpense, setYourExpense] = useState<number>(0);
+    const [friendExpense, setFriendExpense] = useState<number>(0);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        const id = e.target.id;
+
+        if (id === "split") setYourExpense(Number(value));
+        else setTotalBill(Number(value));
+    }
+
+    useEffect(() => {
+        setFriendExpense(totalBill - yourExpense);
+    }, [totalBill, yourExpense]);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (totalBill === 0 || yourExpense === 0) return;
+        console.log("Form Submitted");
+    }
+
     return (
-        <form className="form-split-bill">
-            <h2>Split the Bill with x</h2>
+        <form onSubmit={handleSubmit} className="form-split-bill">
+            <h2>Split the Bill with {friend?.name}</h2>
 
             <label htmlFor="split">Bill Value</label>
-            <input type="number" id="split" name="split" />
+            <input onChange={handleChange} value={totalBill} type="number" id="total" name="split" />
+
             <label htmlFor="split">Your Expenses</label>
-            <input type="number" id="split" name="split" />
-            <label htmlFor="split">X's expense</label>
-            <input type="number" id="split" name="split" disabled />
+            <input value={yourExpense} onChange={handleChange} type="number" id="split" name="split" />
+
+            <label htmlFor="split">{friend?.name}'s expense</label>
+            <input value={friendExpense} type="number" id="paying" name="split" disabled />
+
             <label htmlFor="">Who is paying the bill</label>
             <select name="" id="">
                 <option value="you">You</option>
-                <option value="x">X</option>
+                <option value="x">{friend?.name}</option>
             </select>
+
             <Button>Split The Bill</Button>
         </form>
     );
