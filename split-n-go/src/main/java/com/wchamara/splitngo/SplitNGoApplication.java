5e15a299ada5947c570c4f8eacb56ab98b593113
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-23 12:45:17 +0530

    Add CommandLineRunner to populate FriendRepository with dummy data

--- split-n-go/src/main/java/com/wchamara/splitngo/SplitNGoApplication.java
+++ split-n-go/src/main/java/com/wchamara/splitngo/SplitNGoApplication.java
@@ -1,13 +1,38 @@
 package com.wchamara.splitngo;
 
+import com.wchamara.splitngo.domain.Friend;
+import com.wchamara.splitngo.repository.FriendRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class SplitNGoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SplitNGoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SplitNGoApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner commandLineRunner(FriendRepository friendRepository) {
+        return args -> {
+            Faker faker = new Faker();
+            String name;
+            String profilePicture;
+            List<Friend> friends = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                name = faker.name().firstName();
+                UUID uuid = UUID.randomUUID();
+                profilePicture = "https://i.pravatar.cc/48?u=%s".formatted(uuid.toString());
+                friends.add(new Friend(name, profilePicture));
+            }
+            friendRepository.saveAll(friends);
+        };
+    }
 }
