116b8f98b859cdbc47d966f4cce9e9dd34d78c9a
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-23 08:47:37 +0530

    move front end code to new folder

---
+++ split-n-go/src/main/java/com/wchamara/splitngo/SplitNGoApplication.java
@@ -0,0 +1,13 @@
+package com.wchamara.splitngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitNGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitNGoApplication.class, args);
+	}
+
+}
